1f62c2186935d0eeddcfb55dc3771ede5cb8cb3f
Thijsvl12 <thijsvl12@gmail.com>
2022-06-14 22:34:51 +0200

    Add base Next Auth login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps, NextPage } from 'next';
+import { BuiltInProviderType } from 'next-auth/providers';
+import { ClientSafeProvider, getProviders, LiteralUnion, signIn, signOut, useSession } from 'next-auth/react';
 
-const Home: NextPage = () => {
-  return <></>;
+interface Props {
+  providers: Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null;
+}
+
+const Home: NextPage<Props> = ({ providers }) => {
+  const { data: session, status } = useSession();
+
+  return (
+    <>
+      {session ? (
+        <>
+          <h1>Welcome {session?.user?.name}</h1>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </>
+      ) : (
+        providers &&
+        Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
+          </div>
+        ))
+      )}
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  return {
+    props: { providers: await getProviders() },
+  };
 };
 
 export default Home;
